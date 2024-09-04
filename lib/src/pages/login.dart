f02132ece8df5ca2e237f12c4ec6fa5eaeb0b69c
jessica <jessymontano@hotmail.com>
2024-09-04 14:44:43 -0700

    agregar logo a login y signup

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -10,15 +10,30 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 FirebaseFirestore db = FirebaseFirestore.instance;
 
 class Login extends StatelessWidget {
-  const Login({super.key});
+  final String? successMessage;
+  const Login({super.key, this.successMessage});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Scaffold(
-            body: Row(
-      children: [LoginForm()],
-    )));
+    return MaterialApp(home: Scaffold(
+      body: Builder(
+        builder: (context) {
+          if (successMessage != null) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(successMessage!)),
+              );
+            });
+          }
+
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [LoginForm(), Image.asset('logo.jpg')],
+          );
+        },
+      ),
+    ));
   }
 }
 
@@ -39,123 +54,126 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'INICIAR SESIÓN',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ),
-          Input(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ingrese su usuario';
-                }
-                return null;
-              },
-              controller: _usernameController,
-              hintText: 'Ingrese el usuario',
-              labelText: 'Usuario'),
-          Input(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ingrese su contraseña';
-                }
-                return null;
-              },
-              controller: _passwordController,
-              hintText: 'Ingrese la contraseña.',
-              labelText: 'Contraseña'),
-          if (_errorMessage.isNotEmpty)
-            Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: Text(
-                  _errorMessage,
-                  style: TextStyle(color: Colors.red),
-                )),
-          Row(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(100, 0, 30, 0),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              SizedBox(
-                width: 200,
-                child: CheckboxListTile(
-                  value: _rememberMe,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      _rememberMe = value!;
-                    });
-                  },
-                  title: const Text('Recuérdame'),
-                  controlAffinity: ListTileControlAffinity.leading,
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'INICIAR SESIÓN',
+                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
               ),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ChangePassword()));
+              Input(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Ingrese su usuario';
+                    }
+                    return null;
                   },
-                  child: const Text('Olvide mi contraseña.')),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Button(
-              onPressed: () async {
-                if (_formKey.currentState?.validate() ?? false) {
-                  final user = await db
-                      .collection('usuarios')
-                      .where('username', isEqualTo: _usernameController.text)
-                      .get();
-                  if (user.docs.isEmpty) {
-                    setState(() {
-                      _errorMessage = 'El ususario no existe.';
-                    });
-                  } else {
-                    final doc = user.docs.first;
-                    if (doc['password'] == _passwordController.text) {
-                      setState(() {
-                        _errorMessage = '';
+                  controller: _usernameController,
+                  hintText: 'Ingrese el usuario',
+                  labelText: 'Usuario'),
+              Input(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Ingrese su contraseña';
+                    }
+                    return null;
+                  },
+                  controller: _passwordController,
+                  hintText: 'Ingrese la contraseña.',
+                  labelText: 'Contraseña'),
+              if (_errorMessage.isNotEmpty)
+                Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: Text(
+                      _errorMessage,
+                      style: TextStyle(color: Colors.red),
+                    )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    width: 200,
+                    child: CheckboxListTile(
+                      value: _rememberMe,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _rememberMe = value!;
+                        });
+                      },
+                      title: const Text('Recuérdame'),
+                      controlAffinity: ListTileControlAffinity.leading,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => const Home()));
-                      });
-                    } else {
-                      setState(() {
-                        _errorMessage = 'Contraseña incorrecta';
-                      });
+                                builder: (context) => const ChangePassword()));
+                      },
+                      child: const Text('Olvide mi contraseña.')),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Button(
+                  onPressed: () async {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      final user = await db
+                          .collection('usuarios')
+                          .where('username',
+                              isEqualTo: _usernameController.text)
+                          .get();
+                      if (user.docs.isEmpty) {
+                        setState(() {
+                          _errorMessage = 'El ususario no existe.';
+                        });
+                      } else {
+                        final doc = user.docs.first;
+                        if (doc['password'] == _passwordController.text) {
+                          setState(() {
+                            _errorMessage = '';
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Home()));
+                          });
+                        } else {
+                          setState(() {
+                            _errorMessage = 'Contraseña incorrecta';
+                          });
+                        }
+                      }
                     }
-                  }
-                }
-              },
-              text: 'Iniciar Sesión',
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text('¿No tienes una cuenta?'),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUp()));
                   },
-                  child: const Text('Registrate'))
+                  text: 'Iniciar Sesión',
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('¿No tienes una cuenta?'),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUp()));
+                      },
+                      child: const Text('Registrate'))
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
--- lib/src/pages/signup.dart
+++ lib/src/pages/signup.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../widgets/button.dart';
 import '../widgets/input.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'login.dart';
 
 FirebaseFirestore db = FirebaseFirestore.instance;
 
@@ -10,10 +11,12 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         home: Scaffold(
             body: Row(
-      children: [SignUpForm()],
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [SignUpForm(), Image.asset('logo.jpg')],
     )));
   }
 }
@@ -37,98 +40,117 @@ class _SignUpFormState extends State<SignUpForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'REGÍSTRATE',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ),
-          Input(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ingrese un nombre de usuario';
-                }
-                return null;
-              },
-              controller: _usernameController,
-              hintText: 'Ingrese el nombre de usuario',
-              labelText: 'Nombre de usuario'),
-          Input(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ingrese su nombre completo';
-                }
-                return null;
-              },
-              controller: _nameController,
-              hintText: 'Ingrese su nombre completo',
-              labelText: 'Nombre completo'),
-          Input(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ingrese su email';
-                } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                  return 'Ingrese un email válido';
-                }
-                return null;
-              },
-              controller: _emailController,
-              hintText: 'ejemplo@gmail.com',
-              labelText: 'Correo electrónico'),
-          Input(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ingrese una contraseña';
-                } else if (value != _repeatPasswordController.text) {
-                  return 'Las contraseñas deben ser iguales';
-                }
-                return null;
-              },
-              controller: _passwordController,
-              hintText: 'Ingrese su contraseña',
-              labelText: 'Contraseña'),
-          Input(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Repita su contraseña';
-                } else if (value != _passwordController.text) {
-                  return 'Las contraseñas deben ser iguales';
-                }
-                return null;
-              },
-              controller: _repeatPasswordController,
-              hintText: 'Vuelva a ingresar su contraseña',
-              labelText: 'Confirmar contraseña'),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Button(
-              onPressed: () async {
-                if (_formKey.currentState?.validate() ?? false) {
-                  final user = <String, dynamic>{
-                    'username': _usernameController.text,
-                    'name': _nameController.text,
-                    'email': _emailController.text,
-                    'password': _passwordController.text,
-                    'role': 'usuario'
-                  };
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(100, 0, 30, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'REGÍSTRATE',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+              Input(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Ingrese un nombre de usuario';
+                    }
+                    return null;
+                  },
+                  controller: _usernameController,
+                  hintText: 'Ingrese el nombre de usuario',
+                  labelText: 'Nombre de usuario'),
+              Input(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Ingrese su nombre completo';
+                    }
+                    return null;
+                  },
+                  controller: _nameController,
+                  hintText: 'Ingrese su nombre completo',
+                  labelText: 'Nombre completo'),
+              Input(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Ingrese su email';
+                    } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                        .hasMatch(value)) {
+                      return 'Ingrese un email válido';
+                    }
+                    return null;
+                  },
+                  controller: _emailController,
+                  hintText: 'ejemplo@gmail.com',
+                  labelText: 'Correo electrónico'),
+              Input(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Ingrese una contraseña';
+                    } else if (value != _repeatPasswordController.text) {
+                      return 'Las contraseñas deben ser iguales';
+                    }
+                    return null;
+                  },
+                  controller: _passwordController,
+                  hintText: 'Ingrese su contraseña',
+                  labelText: 'Contraseña'),
+              Input(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Repita su contraseña';
+                    } else if (value != _passwordController.text) {
+                      return 'Las contraseñas deben ser iguales';
+                    }
+                    return null;
+                  },
+                  controller: _repeatPasswordController,
+                  hintText: 'Vuelva a ingresar su contraseña',
+                  labelText: 'Confirmar contraseña'),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Button(
+                  onPressed: () async {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      final user = <String, dynamic>{
+                        'username': _usernameController.text,
+                        'name': _nameController.text,
+                        'email': _emailController.text,
+                        'password': _passwordController.text,
+                        'role': 'usuario'
+                      };
 
-                  db.collection('usuarios').add(user).then(
-                      (DocumentReference doc) =>
-                          print('document snapshot added'));
-                  Navigator.pop(context);
-                }
-              },
-              text: 'Crear Cuenta',
-            ),
+                      db.collection('usuarios').add(user).then(
+                          (DocumentReference doc) =>
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Usuario creado exitosamente'),
+                                ),
+                              ));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Login(
+                                    successMessage:
+                                        'Usuario registrado correctamente.',
+                                  )));
+                    }
+                  },
+                  text: 'Crear Cuenta',
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const Login()));
+                  },
+                  child: const Text('Cancelar'))
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
